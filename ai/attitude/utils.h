b6c43ed06cd52e9a55d1a978c19a6bc0958b6ea6
zakri <i1abnrk@yahoo.com>
2015-02-19 16:24:21 -0600

    calc_halflife complete

--- ai/attitude/utils.h
+++ ai/attitude/utils.h
@@ -11,5 +11,5 @@
 const char *strtoupper(const char *src);
 /* Calculate a halflife as v' = floor(v*pow(0.5, (t/h))) */
 int calc_halflife(int value, int halflife, int turns);
-
+/*compare 2 universals by universal_number*/
 int universalcmp(const struct universal *u1, const struct universal *u2);
